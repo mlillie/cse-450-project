408b18af62b0a48ca7b7e27947161f033c43a7d8
pwitulsk <pwitulsk@asu.edu>
2018-11-25 12:27:53 -0700

    Plotted swap count vs time. Added pref lists to array for analysis.

--- main.py
+++ main.py
@@ -14,19 +14,42 @@ from algorithm import Algorithm
 from data import Data
 from analysis import Analysis
 from matplotlib import pyplot
+import datetime
+from matplotlib.dates import date2num
 
 if __name__ == '__main__':
 
+    file_array = []
     # will add an iterator which will pass all files in test_cases folder to Data
-    # file = "3_pref_lists"
-    # file = "5_pref_lists"
-    # file = "10_pref_lists"
-    file = "\\test_cases\Sample2.xlsx"
-    # save the preference list read from the excel file
-    data = Data().read(file)
+    file_3 = "\\test_cases\\3_pref_lists.xlsx"
+    file_5 = "\\test_cases\\5_pref_lists.xlsx"
+    file_10 = "\\test_cases\\10_pref_lists.xlsx"
+    file_50 = "\\test_cases\\50_pref_lists.xlsx"
+    file_100 = "\\test_cases\\100_pref_lists.xlsx"
 
-    algorithm = Algorithm()
-    algorithm.doctor_matching_algorithm(data, Data().calc_percentages(data))
+    file_array.append(file_3)
+    file_array.append(file_5)
+    file_array.append(file_10)
+    file_array.append(file_50)
+    file_array.append(file_100)
+
+    # save the preference list read from the excel file
+    y_points = []
+    time_points = []
+
+    for file in file_array:
+        data = Data().read(file)
+        start_time = datetime.datetime.now() # get the start time
+        algorithm = Algorithm()
+        algorithm.doctor_matching_algorithm(data, Data().calc_percentages(data))
+        end_time = datetime.datetime.now() # get the end time
+        total_time = end_time - start_time
+        time_points.append(total_time.microseconds)
+        y_points.append(algorithm.get_swap_count())
+
+        print("Total time: " + str(total_time) + '\n')
+        print("Total swap count: " + str(algorithm.get_swap_count()) + '\n')
+
     # Algorithm().patient_matching_algorithm(data, Data().calc_percentages(data))
     # then the returned preference lists from Data will be given to Algorithms
     # Analysis will need to monitor the algorithm
@@ -35,7 +58,7 @@ if __name__ == '__main__':
 
     print('Analyzing Algorithms...')
 
-    print("Total swap count: " + str(algorithm.get_swap_count()) + '\n')
+    #print("Total swap count: " + str(algorithm.get_swap_count()) + '\n')
 
     #analysis.plotTC(analysis.fconst, 10, 1000, 10, 10)
     #analysis.plotTC(analysis.flinear, 10, 1000, 10, 10)
@@ -44,4 +67,10 @@ if __name__ == '__main__':
     #analysis.plotTC(analysis.fsort, 10, 1000, 10, 10)
 
     # show plot
+    pyplot.title('Time Complexity Analysis')
+    pyplot.xlabel('Time to execute (microseconds)')
+    pyplot.ylabel('# of swaps')
+    #dates = date2num(time_points)
+    pyplot.plot(time_points, y_points)
+    #pyplot.gcf().autofmt_xdate()
     pyplot.show()
